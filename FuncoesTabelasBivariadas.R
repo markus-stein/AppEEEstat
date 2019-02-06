a1b88d41efcb8ee52542e64a3fda08a925a9fcb0
ArthurRasbold <45044634+ArthurRasbold@users.noreply.github.com>
2019-02-06 13:02:15 -0200

    6 de fevereiro
    Arquivo organizado com descrições

--- FuncoesTabelasBivariadas.R
+++ FuncoesTabelasBivariadas.R
@@ -4,25 +4,28 @@
 
 ###################################### funções auxiliares ############################
 
+#### Criadora de intervalos de classes ####
 QuantAux <- function(y){
   histy <- hist(as.numeric(y), plot=F)
   intervclasse <- cut(y, breaks = histy$breaks, include.lowest=TRUE)
   return(intervclasse)
 }
 
-
+#### Adiciona total de uma coluna
 TotalColuna <- function(x){
   totalcoluna <- colSums(x)
   x <- rbind(x, totalcoluna)
   return(x)
 }
 
+#### Adiciona total de uma linha
 TotalLinha <- function(y){
   totallinha <- rowSums(y)
   y <- cbind(y, totallinha)
   return(y)
 }
 
+#### Adiciona total de uma linha e de uma coluna
 TotalColLin <- function(x){
   totalcol <- TotalColuna(x)
   totalcollin <- TotalLinha(totalcol)
@@ -30,126 +33,63 @@ TotalColLin <- function(x){
   return(totalcollin)
 }
 
-# PropPorColuna <- function(w,z){
-#   Porcoluna <- table(w,z)
-#   Porcoluna <- prop.table(Porcoluna,2)
-#   return(Porcoluna)
-# }
 
-# PropPorLinha <- function(w,z){
-#   Porlinha <- table(w,z)
-#   Porlinha <- prop.table(Porlinha,1)
-#   return(Porlinha)
-# }
-
-########################## Qualitativa Qualitativa ####################################
-
-# Valor absoluto
-TabelaBiQuali <- function(w,z){
-  TabiQuali <- table(w,z)
-  TabiQuali <- TotalColLin(TabiQuali)
+################## Frequência Absoluta ####################### 
+TabelaQlQl <- function(w,z){
+  TabiQlQl <- table(w,z)
+  TabiQlQl <- TotalColLin(TabiQlQl)
   return(TabiQuali)
 }
-TabelaBiQuali(mtcars$vs,mtcars$am)
 
-  
-
-  # Frequência relativa   
-TabelaBiQualifr <- function(w,z){
-  TabiQualifr <- table(w,z)/length(w)
-  TabiQualifr <- TotalColLin(TabiQualifr)
-  return(TabiQualifr)
-}
-TabelaBiQualifr(mtcars$vs,mtcars$am)
-
-  
-  
-  # Proporção em relação à coluna
-PropPorColuna(mtcars$vs,mtcars$am)
-
-
-  # Proporção em relação à linha
-PropPorLinha(mtcars$vs,mtcars$am)
-
-
-################## QUALI QUANT ######################################################  
-  
-# A primeira variável deve ser qualitativa e a segunda deve ser quantitativa
-# Valor absoluto
 TabelaQlQn  <- function(w,z){
   TabelaQualiQuant <- table(w, QuantAux(z))
   TabelaQualiQuant <- TotalColLin(TabelaQualiQuant)
   return(TabelaQualiQuant)
 }
-TabelaQlQn(mtcars$vs,mtcars$wt)
 
 
-# Frequência relativa
-TabelaQlQnfr <- function(w,z){
-  TabelaQlQnfr <- table(w, QuantAux(z))/length(w)
-  TabelaQlQnfr <- TotalColLin(TabelaQlQnfr)
-  return(TabelaQlQnfr)
+TabelaQnQl  <- function(w,z){
+  TabelaQnQl <- table(QuantAux(w),z)
+  TabelaQnQl <- TotalColLin(TabelaQnQl)
+  return(TabelaQualiQuant)
 }
-TabelaQlQnfr(mtcars$vs,mtcars$wt)
- 
 
 
-# Proporção em relação à coluna
-# TabelaQlQnProCol <- function(w,z){
-#   TabelaQlQncol <- PropPorColuna(w,QuantAux(z))
-#   return(TabelaQlQncol)
-#   }
-# TabelaQlQnProCol(mtcars$vs, mtcars$wt)
-PropPorColuna(mtcars$vs, QuantAux(mtcars$wt))
-
-
-# Proporção em relação à linha
-# TabelaQlQnProLin <- function(w,z){
-#   TabelaQlQnlin <- PropPorLinha(w,QuantAux(z))
-#   return(TabelaQlQnlin)
-# }
-# TabelaQlQnProLin(mtcars$vs, mtcars$wt)
-PropPorLinha(mtcars$vs, QuantAux(mtcars$wt))
-
-
-#################### Quantitativa Quantitativa ##########################
-
-# Valor absoluto
 TabelaQnQn <- function(w,z){
   Quantit <- table(QuantAux(w),QuantAux(z))
   Quantit <- TotalColLin(Quantit)
   return(Quantit)
 }
-TabelaQnQn(mtcars$wt,mtcars$qsec)
+
+#################### Frequência Relativa ##########################
 
 
+TabelaQlQlfr <- function(w,z){
+  TabiQlQlfr <- table(w,z)/length(w)
+  TabiQlQlfr <- TotalColLin(TabiQlQlfr)
+  return(TabiQualifr)
+}
 
-# Frequência relativa
+
+TabelaQlQnfr <- function(w,z){
+  TabelaQlQnfr <- table(w, QuantAux(z))/length(w)
+  TabelaQlQnfr <- TotalColLin(TabelaQlQnfr)
+  return(TabelaQlQnfr)
+}
+
+
+TabelaQnQlfr <- function(w,z){
+  TabelaQnQlfr <- table(QuantAux(w), z)/length(w)
+  TabelaQnQlfr <- TotalColLin(TabelaQnQlfr)
+  return(TabelaQlQnfr)
+}
+
+
 TabelaQnQnfr <- function(w,z){
   Quantitfr <- table(QuantAux(w),QuantAux(z))/length(w)
   Quantitfr <- TotalColLin(Quantitfr)
   return(Quantitfr)
 }
-TabelaQnQnfr(mtcars$wt,mtcars$qsec)
-
-
- #  Proporção em relação à coluna
-# TabelaQnQnProCol <- function(w,z){
-#   Quantitcol <- PropPorColuna(QuantAux(w),QuantAux(z))
-#   return(Quantitcol)
-# }
-# TabelaQnQnProCol(mtcars$wt,mtcars$qsec)
-PropPorColuna(QuantAux(mtcars$wt),QuantAux(mtcars$qsec))
-
-
-  # Proporção em relação à linha
-# TabelaQnQnProLin <- function(w,z){
-#   Quantitlin <- PropPorLinha(QuantAux(w),QuantAux(z))
-#   return(Quantitlin)
-# }
-# TabelaQnQnProLin(mtcars$wt, mtcars$qsec)
-PropPorLinha(QuantAux(mtcars$wt),QuantAux(mtcars$qsec))
-
 
 
 ####################### Proporção por Coluna ####################################
@@ -166,13 +106,18 @@ PropPorColunaQlQn <- function(w,z){
   return(Porcoluna)
 }
 
+PropPorColunaQnQl <- function(w,z){
+  Porcoluna <- table(QuantAux(w),z)
+  Porcoluna <- prop.table(Porcoluna,2)
+  return(Porcoluna)
+}
+
 PropPorColunaQnQn <- function(w,z){
   Porcoluna <- table(QuantAux(w),QuantAux(z))
   Porcoluna <- prop.table(Porcoluna,2)
   return(Porcoluna)
 }
 
-PropPorColunaQlQn(mtcars$vs, mtcars$wt)
 
 
 ############################ Proporção por Linha ###########################
@@ -189,6 +134,12 @@ PropPorLinhaQlQn <- function(w,z){
   return(Porlinha)
 }
 
+PropPorLinhaQnQl <- function(w,z){
+  Porlinha <- table(QuantAux(w),z)
+  Porlinha <- prop.table(Porlinha,1)
+  return(Porlinha)
+}
+
 PropPorLinhaQnQn <- function(w,z){
   Porlinha <- table(QuantAux(w),QuantAux(z))
   Porlinha <- prop.table(Porlinha,1)
@@ -197,92 +148,141 @@ PropPorLinhaQnQn <- function(w,z){
 
 
 
+######################## Tabela bivariada final ##########
 
 
 TabelaBivariada <- function(w,z){
   if (is.factor(w) & is.factor(z)){
-    TabelaBiQuali(w,z)
-  } else { 
-  if (is.factor(w) & !is.factor(z)){
-    TabelaQlQn(w,z)
-  } else {
-    TabelaQnQn(w,z)
-  }
+    TabelaQlQl(w,z)
+      } else { 
+        if (is.factor(w) & !is.factor(z)){
+          TabelaQlQn(w,z)
+            } else {
+              if (!is.factor(w) & is.factor(z)){
+                TabelaQnQl(w,z)
+                  } else {
+                    TabelaQnQn(w,z)
+      }
+    }
   }
 }
-TabelaBivariada(mtcars$cyl,mtcars$qsec)
-
+######################## Tabela bivariada relativa final ##########
 
 TabelaBivariadaRelativa <- function(w,z){
   if (is.factor(w) & is.factor(z)){
-    TabelaBiQualifr(w,z)
-  } else { 
-    if (is.factor(w) & !is.factor(z)){
-      TabelaQlQnfr(w,z)
-    } else {
-      TabelaQnQnfr(w,z)
-    }
+    TabelaQlQlfr(w,z)
+      } else { 
+        if (is.factor(w) & !is.factor(z)){
+          TabelaQlQnfr(w,z)
+            } else {
+              if (!is.factor(w) & is.factor(z)){
+                TabelaQnQlfr(w,z)
+                  } else { 
+                    TabelaQnQnfr(w,z)
+   }
   }
-}
-TabelaBiQualifr(mtcars$am,mtcars$cyl)
-
-
-
-TabelaBivariadaPropColuna <- function(w,z){
-  if (is.factor(w) & is.factor(z)){
-    PropPorColuna(w,z)
-  } else { 
-    if (is.factor(w) & !is.factor(z)){
-     PropPorColuna(w,QuantAux(z))
-    } else {
-      PropPorColuna(QuantAux(w),QuantAux(z))
-    }
-  }
-}
-TabelaBivariadaPropColuna(mtcars$vs, mtcars$wt)
-
-
-
-TabelaBivariadaPropLinha <- function(w,z){
-  if (is.factor(w) & is.factor(z)){
-    PropPorLinha(w,z)
-  } else { 
-    if (is.factor(w) & !is.factor(z)){
-      PropPorLinha(w,QuantAux(z))
-    } else {
-      PropPorLinha(QuantAux(w),QuantAux(z))
-    }
-  }
+ }
 }
 
-TabelaBivariadaPropLinha(mtcars$wt, mtcars$qsec)
 
 
 ################################ Proporção pro coluna final ##############
-PropPorColuna <- function(w,z){
+ProporcaoPorColuna <- function(w,z){
   if (is.factor(w) & is.factor(z)){
     PropPorColunaQlQl(w,z) 
-    } else { 
-    if (is.factor(w) & !is.factor(z)){
-      PropPorColunaQlQn(w,z)
-      } else { PropPorColunaQnQn(w,z)}
+      } else { 
+        if (is.factor(w) & !is.factor(z)){
+          PropPorColunaQlQn(w,z)
+            } else { 
+              if (!is.factor(w) & is.factor(z)){
+                PropPorColunaQnQl
+                  } else {
+                    PropPorColunaQnQn(w,z)}
     }
   }
-
+}
 PropPorColuna(mtcars$qsec,mtcars$wt)  
 
 
-#################################### Proporção por coluna final ##################
-PropPorLinha <- function(w,z){
+#################################### Proporção por linha final ##################
+ProporcaoPorLinha <- function(w,z){
   if (is.factor(w) & is.factor(z)){
     PropPorLinhaQlQl(w,z) 
-  } else { 
-    if (is.factor(w) & !is.factor(z)){
-      PropPorLinhaQlQn(w,z)
-    } else { PropPorLinhaQnQn(w,z)}
+      } else { 
+        if (is.factor(w) & !is.factor(z)){
+          PropPorLinhaQlQn(w,z)
+            } else { 
+              if (!is.factor(w) & is.factor(z)){
+                PropPorLinhaQnQl
+                  } else {
+                    PropPorLinhaQnQn(w,z)}
+    }
   }
-  }
+}
+PropPorLinha(mtcars$wt,mtcars$qsec)  
 
-PropPorLinha(mtcars$am,mtcars$vs)  
+##################################### Novas funções ###########################
 
-
+# TabelaBi <- function(w,z){
+#   Tabelabi <- table(w,z)
+#   Tabelabi <- TotalColLin(Tabelabi)
+#   return(Tabelabi)
+# }
+# 
+# 
+# 
+# TabelaBifr <- function(w,z){
+#   Tabelabifr <- table(w,z)/length(x)
+#   Tabelabifr <- TotalColLin(Tabelabifr)
+#   return(Tabelabifr)
+# }
+# 
+# 
+# PropPorColuna <- function(w,z){
+#   Porcoluna <- table(w,z)
+#   Porcoluna <- prop.table(Porcoluna,2)
+#   return(Porcoluna)
+# }
+# 
+# 
+# PropPorLinha <- function(w,z){
+#   Porlinha <- table(w,z)
+#   Porlinha <- prop.table(Porlinha,1)
+#   return(Porlinha)
+# }
+# 
+# 
+# TabelaBivariada <- function(w,z,arg = c(1,2,3,4)){
+#  if (!is.factor(w)){
+#   a <- QuantAux(w)
+#    } else {
+#     if (!is.factor(z)){
+#      b <- QuantAux(z)
+#       } else {
+#         if(is.factor(w)){
+#           a <- w
+#            } else {
+#             if(is.factor(z)){
+#              b <- z
+#               } else {
+#                if (arg == 1){
+#                 TabelaBi(a,b)
+#                  } else {
+#                    if(arg == 2){
+#                      TabelaBifr(a,b)
+#                       } else {
+#                        if(arg == 3){
+#                         PropPorColuna(a,b)
+#                          } else {
+#                           if(arg == 4){
+#                            PropPorLinha(a,b)
+#                           }
+#        }
+#       }
+#      }
+#     }
+#    }
+#   }
+#  }
+# }
+# 
