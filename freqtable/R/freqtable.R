64a035710af2fca3fb046a45c7d443a4cefba03e
markus-stein <markus.stein@ufrgs.br>
2019-10-22 13:57:11 -0300

    R package 'freqtable'

---
+++ freqtable/R/freqtable.R
@@ -0,0 +1,10 @@
+#'  Pacote com funções criadoras de tabelas frequências
+#'
+#'  Contém 7 funções que criam ou auxiliam na criação de tabelas de frequências
+#'
+#'  @docType package
+#'
+#'  @author Arthur Rasbold Neves \email{arthur.rasbold@hotmail.com}
+#'
+#'  @name freqtable
+NULL
